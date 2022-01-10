5b2cdc19f00dea789f482f0b764a8570faa8180f
Prashant Kankhara <prashantkanakhara1988@gmail.com>
2022-01-10 16:49:40 +0530

    Updated code to support exact psuedo code

--- Controllers/BatchController.cs
+++ Controllers/BatchController.cs
@@ -33,8 +33,16 @@ namespace BatchProcessingAPI.Controllers
                 var applicationRepository = new ApplicationRepository();
                 if (applicationRepository.IsProfileExists(request.profileName))
                 {
-                    var baseProfile = new BaseProfile();
-                    baseProfile.ProcessProfile(request);
+                    if (!string.IsNullOrEmpty(request.DataString))
+                    {
+                        var baseProfile = new BaseProfile();
+                        baseProfile.ProcessProfile(request);
+                    }
+                    else
+                    {
+                        var invalidProfileName = new Exception("Empty JSON string found.");
+                        throw invalidProfileName;
+                    }
                 }
                 else
                 {
